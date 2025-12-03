68417fb432c81c632276b96c884b2c5555ffd673
AlexTLDR <alex.badragan@protonmail.com>
2025-12-03 16:25:01 +0100

    feat(main): add blur functionality to image processing
    - Implemented a new subcommand "blur" to apply a blur effect on images.
    - Added dependency on the `image` crate for image manipulation.
    - Updated main function to handle subcommands and arguments.
    - Created a `blur` function to perform the image blur operation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
+    let mut args: Vec<String> = std::env::args().skip(1).collect();
     if args.is_empty() {
         print_usage_and_exit();
+    }
+    let subcommand = args.remove(0);
+    match subcommand.as_str() {
+        "blur" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+
+            blur(infile, outfile);
+        }
+        _ => {
+            print_usage_and_exit();
+        }
     }
 }
 
@@ -11,4 +26,10 @@ fn print_usage_and_exit() {
     println!("fractal OUTFILE");
 
     std::process::exit(-1);
+}
+
+fn blur(infile: String, outfile: String) {
+    let img = image::open(infile).expect("Failed to open INFILE");
+    let img2 = img.blur(3.0);
+    img2.save(outfile).expect("Failed to save OUTFILE");
 }
