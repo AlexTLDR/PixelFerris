5f699ffc0a38a649600b735b896a7fb19cca3a9e
AlexTLDR <alex.badragan@protonmail.com>
2025-12-03 16:53:19 +0100

    feat(main): add fractal generation functionality
    - Introduced a new command for generating fractals.
    - Added dependency on `num-complex` for complex number operations.
    - Implemented fractal generation logic in `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ fn main() {
 
             blur(infile, outfile);
         }
+        "fractal" => {
+            if args.len() != 1 {
+                print_usage_and_exit();
+            }
+            let outfile = args.remove(0);
+            fractal(outfile);
+        }
         _ => {
             print_usage_and_exit();
         }
@@ -32,4 +39,40 @@ fn blur(infile: String, outfile: String) {
     let img = image::open(infile).expect("Failed to open INFILE");
     let img2 = img.blur(3.0);
     img2.save(outfile).expect("Failed to save OUTFILE");
+}
+
+// This code was adapted from https://github.com/image-rs/image?tab=readme-ov-file#generating-fractals
+fn fractal(outfile: String) {
+    let width = 800;
+    let height = 800;
+
+    let mut imgbuf = image::ImageBuffer::new(width, height);
+
+    let scale_x = 3.0 / width as f32;
+    let scale_y = 3.0 / height as f32;
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        // Use red and blue to be a pretty gradient background
+        let red = (0.3 * x as f32) as u8;
+        let blue = (0.3 * y as f32) as u8;
+
+        // Use green as the fractal foreground (here is the fractal math part)
+        let cx = y as f32 * scale_x - 1.5;
+        let cy = x as f32 * scale_y - 1.5;
+
+        let c = num_complex::Complex::new(-0.4, 0.6);
+        let mut z = num_complex::Complex::new(cx, cy);
+
+        let mut green = 0;
+        while green < 255 && z.norm() <= 2.0 {
+            z = z * z + c;
+            green += 1;
+        }
+
+        // Actually set the pixel. red, green, and blue are u8 values!
+        *pixel = image::Rgb([red, green, blue]);
+    }
+
+    imgbuf.save(outfile).unwrap();
 }
